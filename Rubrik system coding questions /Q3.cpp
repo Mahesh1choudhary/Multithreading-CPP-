51370dbd6a9cac516bd9dae6dc331c0d72b73f97
Mahesh Choudhary <98116623+Mahesh1choudhary@users.noreply.github.com>
2024-07-31 09:54:19 +0530

    Update Q3.cpp

--- Rubrik system coding questions /Q3.cpp
+++ Rubrik system coding questions /Q3.cpp
@@ -2,7 +2,8 @@
 /*
 Question link - https://leetcode.com/discuss/interview-experience/1791347/rubrik-sde2-feb-2022-screening-round-rejected
 
-
+Expose an interface which will accept a Runnable object along with the waitTime . The implementation should run the Runnable object after waiting time is over.
+The condition was that, invocation to the interface should not be blocking. Client will invoke the method, and it should exit immediately.
 
 
 
